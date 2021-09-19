d1455a7918a662e2d5c25eab07b7c166ead40f05
moreskoomaplz <vahurtoss@gmail.com>
2021-09-19 16:02:56 +0300

    Add index.html and base algo

---
+++ script/code.js
@@ -0,0 +1,22 @@
+
+let userPlay = prompt("rock, paper or scissors?"); //prompt user for rock, paper or scissors
+//check if valid input
+let computerPlay1, computerPlay2, computerPlay3; //define thee options for computer
+//make random number generator
+//if 1/3 value, return rock
+//if 2/3 value, return paper
+//if 3/3 value, return scissor
+//rock function
+//if computer paper lose, else win
+//paper function
+//if computer rock win, else lose
+//scissors function
+//if computer rock lose, else win
+
+
+
+
+function computerPlay(yourChoice){
+    let option1, option2, option3;
+
+}
