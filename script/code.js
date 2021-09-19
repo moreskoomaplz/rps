dca6f3b863cc80e230f3139b113ed379677bcb0a
moreskoomaplz <vahurtoss@gmail.com>
2021-09-19 19:29:23 +0300

    Solution without looking for any help

--- script/code.js
+++ script/code.js
@@ -1,15 +1,65 @@
 
 let userPlay = prompt("rock, paper or scissors?"); //prompt user for rock, paper or scissors
+    console.log("you played " + userPlay);
 //check if valid input
 let computerPlay1, computerPlay2, computerPlay3; //define thee options for computer
-//make random number generator
-//if 1/3 value, return rock
-//if 2/3 value, return paper
-//if 3/3 value, return scissor
+function rng(){
+    computerOption = Math.floor(Math.random()*3);//make random number generator between 0 and 2
+    if (computerOption < 1){
+        computerPlay = "rock";//if 1/3 value, return rock
+        console.log("computer plays " + computerPlay);
+        if (computerPlay === userPlay){
+            console.log("It's a draw!");
+            }
+        else if (computerPlay === "rock" && userPlay === "scissors"){ //rock function
+            console.log("you lose");
+                }
+                else{
+                    console.log("you win");
+                }
+    }
+    else if (computerOption < 2){//if 2/3 value, return paper
+        computerPlay = "paper";
+        console.log("computer plays " + computerPlay);
+        if (computerPlay === userPlay){
+            console.log("It's a draw!");
+            }
+        else if (computerPlay === "paper" && userPlay === "rock"){ //rock function
+            console.log("you lose");
+                }
+        else{
+                    console.log("you win");
+                }
+    }
+    else {
+        computerPlay = "scissors";//if 3/3 value, return scissor
+        console.log("computer plays " + computerPlay);
+        if (computerPlay === userPlay){
+            console.log("It's a draw!");
+            }
+        else if (computerPlay === "scissors" && userPlay === "rock"){ //rock function
+            console.log("you win");
+                }
+else{
+                    console.log("you lose");
+                }
+    }
+    
+    
+    
+    
+
+}
+
+
+
+
+
+
 //rock function
 //if computer paper lose, else win
 //paper function
-//if computer rock win, else lose
+
 //scissors function
 //if computer rock lose, else win
 
